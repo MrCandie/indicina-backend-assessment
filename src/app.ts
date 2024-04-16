54b2b0e7f3a11c8245c9f08a846e99ea9c164344
mrcandie~ <akinwumiadekanmi2@gmail.com>
2024-04-16 09:03:39 +0100

    still fixing bugs

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.static(dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(dirname + "/public/index.html");
+  res.sendFile(dirname + "../public/index.html");
 });
 
 app.use("/", urlRoute);
